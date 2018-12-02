137863a6c6281fec8c14ea5220ac87b32bcf2324
Kyle Tolle <kyle@nullsix.com>
2018-12-01 22:39:32 -0500

    Updates

---
+++ precache-manifest.f62b18227587e3cbef37c00840abd7f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "510c1e8671cb22475c4a",
+    "url": "/tic_tac_toe-react/static/css/main.ea06ba5a.chunk.css"
+  },
+  {
+    "revision": "510c1e8671cb22475c4a",
+    "url": "/tic_tac_toe-react/static/js/main.510c1e86.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tic_tac_toe-react/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "dcd067bcaa1ef47030c6",
+    "url": "/tic_tac_toe-react/static/js/runtime~main.dcd067bc.js"
+  },
+  {
+    "revision": "04e0af8994b9f18c90c60645e044a2eb",
+    "url": "/tic_tac_toe-react/index.html"
+  }
+];
